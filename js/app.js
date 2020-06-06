6974730417157bc597273177403b792087f15244
sanjuron <julien_nn@live.fr>
2020-06-06 14:10:21 +0200

    ajout 2 fonctions : handleSubmit & handleInput

--- js/app.js
+++ js/app.js
@@ -2,14 +2,32 @@ class App extends React.Component {
 
     state = {
 
+        name: "Julien",
+
+    }
+
+    handleInput = e => {
+        this.setState({
+            pseudo: e.target.value
+        });
+        console.log(e.target.value);
+
+
+    }
+
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.value);
+        console.log(e);
     }
 
 
     render() {
         return(
-        <form action="">
+        <form action="" onSubmit={this.handleSubmit}>
             <p>Say Something</p>
-            <input type="text" placeholder= "Your Name"/> <br/>
+            <input type="text" placeholder= "Your Name" value={this.pseudo} onChange={this.handleInput}/> <br/>
             <textarea name="" id="" cols="30" rows="10" placeholder="Your Comment"></textarea> <br/>
             <button>Comment -></button>
         </form>
@@ -17,6 +35,5 @@ class App extends React.Component {
     }
 
 }
-
 
 ReactDOM.render(<App/>,document.getElementById("app"));
